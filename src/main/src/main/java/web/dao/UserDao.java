6ea9a04bea98c44c422ee8488f5f5cb80f2f75ef
haiduc34 <61165125+haiduc34@users.noreply.github.com>
2020-07-08 16:07:43 +0300

    Add files via upload

---
+++ src/main/src/main/java/web/dao/UserDao.java
@@ -0,0 +1,17 @@
+package web.dao;
+import web.model.*;
+
+
+import java.util.List;
+
+public interface UserDao {
+    public void addUser(User user);
+
+    public void updateUser(User user);
+
+    public void removeUser(int id);
+
+    public User getUserById(int id);
+
+    public List<User> listUsers();
+}
